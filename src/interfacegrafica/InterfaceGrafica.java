654b7ce11472d6606bab10aa36812e19f04423d9
Douglas Ribeiro Costa <douglasrc2008@gmail.com>
2025-05-13 22:06:20 -0300

    Adicionando Botões na interface

--- src/interfacegrafica/InterfaceGrafica.java
+++ src/interfacegrafica/InterfaceGrafica.java
@@ -1,15 +1,29 @@
 // Parte do Douglas
 package interfacegrafica;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class InterfaceGrafica {
     public static void main(String[] args){
-        JFrame janela = new JFrame();
-
+        JFrame janela = new JFrame("Primeira Janela");
         janela.setSize(300, 200);
-        janela.setBounds(100, 100, 300, 200);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        janela.setTitle("Minha Janela");
+        JPanel painel = new JPanel();
+        painel.setLayout(new FlowLayout());
+
+        JButton botao = new JButton("Alimentar");
+        JButton botao1 = new JButton("Banho");
+        JButton botao2 = new JButton("Brincar");
+        JButton botao3 = new JButton("Veterinária");
+
+        painel.add(botao);
+        painel.add(botao1);
+        painel.add(botao2);
+        painel.add(botao3);
+
+
+        janela.add(painel);
         janela.setVisible(true);
     }
 }
